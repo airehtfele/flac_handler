b3128b56085e8149c16b9a8af53f2f0e2606772a
Mijael Viricochea Parra <mijael.viricochea@outlook.com>
2022-05-29 22:12:39 -0400

    Main App created

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:unflacgui/pages/home_page.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: HomePage(),
+    );
+  }
+}
